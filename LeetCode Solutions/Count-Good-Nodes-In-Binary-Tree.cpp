e431f9696b4b556fa428e948782c3a3326c55a1d
HusseinYasser <96487516+HusseinYasser@users.noreply.github.com>
2022-09-14 08:40:28 +0200

    Add files via upload

---
+++ LeetCode Solutions/Count-Good-Nodes-In-Binary-Tree.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    int solve(TreeNode* root, int mx)
+    {
+        if(root == NULL)
+            return 0;
+        int ans = 0;
+        if(root->val >= mx)
+        {
+            ans++;
+        }
+        mx = max(mx, root->val);
+        return ans + solve(root->left, mx) + solve(root->right, mx);
+    }
+
+    int goodNodes(TreeNode* root) {
+        return solve(root, root->val);
+    }
+};
