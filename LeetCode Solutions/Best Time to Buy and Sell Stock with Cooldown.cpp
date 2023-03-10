867496a26c4688c354b73e06be93236498a74311
HusseinYasser <husseinyasser388@gmail.com>
2023-03-10 15:40:55 -0800

    Best time to buy and sell stock better approach

--- LeetCode Solutions/Best Time to Buy and Sell Stock with Cooldown.cpp
+++ LeetCode Solutions/Best Time to Buy and Sell Stock with Cooldown.cpp
@@ -2,23 +2,16 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         //dp 
-        vector<vector<int>> tab(prices.size()+2, vector<int>(1000+2, 0));
+        vector<vector<int>> tab(prices.size()+2, vector<int>(2, 0));
         
         for(int i = prices.size() - 1; i > -1; --i)
         {
-            for(int j = 0; j <= 1001; ++j)
-            {
-                if(j == 1001)
-                {
-                    tab[i][j] = max(tab[i+1][prices[i]], tab[i][j]);
-                }
-                else if(prices[i] >= j)
-                {
-                    tab[i][j] = max(prices[i] - j + tab[i+2][1001], tab[i][j]);
-                }
-                tab[i][j] = max(tab[i][j], tab[i+1][j]);
-            }
+            tab[i][0] = max(tab[i][0], tab[i+1][0]);
+            tab[i][0] = max(tab[i][0], -prices[i] + tab[i+1][1]);
+            tab[i][1] = max(tab[i][1], tab[i+1][1]);
+            tab[i][1] = max(tab[i][1], prices[i] + tab[i+2][0]);
+            
         }
-        return tab[0][1001];
+        return tab[0][0];
     }
 };
